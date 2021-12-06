a8140b1378d44f18411194667313e78272d29e08
AMathMonkey <amathmonkey@gmail.com>
2021-12-06 08:24:10 -0500

    Add day6 solutions

---
+++ day6/day6p1.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+use Data::Dumper;
+use v5.30.0;
+
+my $inputfile = IO::File->new('./input.txt');
+
+my $input = <$inputfile>;
+chomp $input;
+
+my @fish = split ',', $input;
+
+for (1..80) {
+    my @newfish;
+    foreach my $fish (@fish) {
+        if ($fish == 0) {
+            push @newfish, 6, 8
+        } else {
+            push @newfish, $fish - 1
+        }
+    }
+    @fish = @newfish;
+}
+
+say scalar @fish
---
+++ day6/day6p2.pl
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+use Data::Dumper;
+use List::Util qw(sum);
+use v5.30.0;
+
+my $inputfile = IO::File->new('./input.txt');
+
+my $input = <$inputfile>;
+chomp $input;
+
+my @fish = split ',', $input;
+
+my %timers;
+for my $fish (@fish) {
+    $timers{$fish} += 1;
+}
+
+for (1..256) {
+    my %newtimers;
+    while (my ($time, $fish) = each %timers) {
+        if ($time == 0) {
+            $newtimers{6} += $fish;
+            $newtimers{8} += $fish;
+        }
+        else {
+            $newtimers{$time - 1} += $fish;
+        }
+    }
+    %timers = %newtimers;
+}
+
+say sum(values %timers)
