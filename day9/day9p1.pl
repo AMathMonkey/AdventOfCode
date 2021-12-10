43dcc77d0cf0a2b867eab36e8004ca1bba557499
AMathMonkey <amathmonkey@gmail.com>
2021-12-09 21:13:45 -0500

    Slight tweak to use 9 instead of -1 for padding

--- day9/day9p1.pl
+++ day9/day9p1.pl
@@ -12,12 +12,12 @@ chomp @input;
 
 my $grid = pdl map {[split '', $_]} @input;
 
-$grid->whereND($grid == 0) .= -2;
+$grid->whereND($grid == 0) .= -1;
 
 $grid = $grid->range(ndcoords($grid) - 1, 3, 'truncate');
 
-$grid->whereND($grid == 0) .= -1;
-$grid->whereND($grid == -2) .= 0;
+$grid->whereND($grid == 0) .= 9;
+$grid->whereND($grid == -1) .= 0;
 
 my ($rows, $cols, $gridrows, $gridcols) = dims $grid;
 
@@ -25,11 +25,11 @@ my $result = zeroes $rows, $cols;
 
 for (my $i = 0; $i < $rows; $i++) {
     for (my $j = 0; $j < $cols; $j++) {
-        my $min = 10;
+        my $min = 9;
         for (my $gi = 0; $gi < $gridrows; $gi++) {
             for (my $gj = 0; $gj < $gridcols; $gj++) {
                 my $elem = $grid(($i), ($j), ($gi), ($gj));
-                $min = $elem if $elem < $min && $elem > -1;
+                $min = $elem if $elem < $min
             }
         }
         my $middle = $grid(($i), ($j), (1), (1));
