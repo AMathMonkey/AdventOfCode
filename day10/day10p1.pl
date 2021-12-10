d0dd4992a0918c0d2b00c517207d8e0b21ecff64
AMathMonkey <amathmonkey@gmail.com>
2021-12-10 10:38:43 -0500

    Add day10 solutions, I'm glad today was pretty easy

---
+++ day10/day10p1.pl
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+use Data::Dumper;
+use v5.30.0;
+
+my $inputfile = IO::File->new('./input.txt');
+
+my @input = <$inputfile>;
+chomp @input;
+
+my %pairs = (
+    '{' => '}',
+    '(' => ')',
+    '[' => ']',
+    '<' => '>'
+);
+
+my %values = (
+    ')' => 3,
+    ']' => 57,
+    '}' => 1197,
+    '>' => 25137 
+);
+
+my $score = 0;
+
+LINE: foreach my $line (@input) {
+    my @stack;
+    foreach my $char (split '', $line) {
+        if ('{([<' =~ /\Q$char/) {
+            push @stack, $char
+        } else {
+            my $last = pop @stack;
+            if ($pairs{$last} ne $char) {
+                $score += $values{$char};
+                next LINE
+            }
+        }
+    }
+} 
+
+say $score;
---
+++ day10/day10p2.pl
@@ -0,0 +1,58 @@
+use strict;
+use warnings;
+use Data::Dumper;
+use v5.30.0;
+
+my $inputfile = IO::File->new('./input.txt');
+
+my @input = <$inputfile>;
+chomp @input;
+
+my %pairs = (
+    '{' => '}',
+    '(' => ')',
+    '[' => ']',
+    '<' => '>'
+);
+
+my %values = (
+    ')' => 1,
+    ']' => 2,
+    '}' => 3,
+    '>' => 4 
+);
+
+my @validLines;
+
+LINE: foreach my $line (@input) {
+    my @stack;
+    foreach my $char (split '', $line) {
+        if ('{([<' =~ /\Q$char/) {
+            push @stack, $char
+        } else {
+            my $last = pop @stack;
+            next LINE if $pairs{$last} ne $char;
+        }
+    }
+    push @validLines, $line;
+} 
+
+my @scores;
+
+foreach my $line (@validLines) {
+    my @stack;
+    foreach my $char (split '', $line) {
+        if ('{([<' =~ /\Q$char/) {
+            push @stack, $char
+        } 
+        else {pop @stack}
+    }
+    my $score = 0;
+    foreach my $char (reverse @stack) {
+        $score *= 5;
+        $score += $values{$pairs{$char}}
+    }
+    push @scores, $score;
+} 
+
+say((sort {$a <=> $b} @scores)[int(@scores / 2)])
