ecfc99d0b4a50254b5a4ce45b4dbdd1ed40a3ed6
AMathMonkey <amathmonkey@gmail.com>
2021-12-17 13:11:59 -0500

    Add day 17 solutions, glad this one was easier

---
+++ day17/day17p2.pl
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+use Data::Dumper;
+use v5.30.0;
+use List::Util qw(max);
+
+my $inputfile = IO::File->new('./input.txt');
+
+my $input = <$inputfile>;
+chomp $input;
+
+my ($xlower, $xupper, $ylower, $yupper) = 
+    $input =~ /target area: x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)/;
+
+sub testvels { my ($xvel, $yvel) = @_;
+    my ($x, $y) = (0, 0);
+    my $maxy = 0;
+    while (1) {
+        $x += $xvel;
+        $y += $yvel;
+        $maxy = max($maxy, $y);
+        $xvel += 0 <=> $xvel;
+        $yvel -= 1;
+
+        my $xInRange = $x >= $xlower && $x <= $xupper;
+        my $yInRange = $y >= $ylower && $y <= $yupper;
+        return $maxy if $xInRange && $yInRange;
+        return -1 if !$xInRange && $xvel == 0;
+        return -1 if $yvel < 0 && $y < $ylower;
+    }
+}
+
+my $result = 0;
+
+for my $xvel (1 .. abs $xupper * 2) {
+    for my $yvel (-abs $yupper * 2 .. abs $yupper * 2) {
+        $result += 1 if testvels($xvel, $yvel) >= 0;
+    }
+}
+
+say $result;
