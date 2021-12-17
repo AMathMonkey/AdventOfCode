51c43be0542979f80164fbfdbde7a34510ec6c63
AMathMonkey <amathmonkey@gmail.com>
2021-12-17 18:46:57 -0500

    Add day16p2 solution based on p1 solution I determined why my p1 approach wasn't working, I needed to count bits parsed by recursive child calls in the parent bitsParsed variable, or else it would exceed its bit limit and read too many subpackets. Made the recursive function return its bits parsed along with its numbers back to its caller. So glad it worked!

---
+++ day16/day16p2-originalApproach.pl
@@ -0,0 +1,91 @@
+use strict;
+use warnings;
+use Data::Dumper;
+use v5.30.0;
+use Sub::Recursive;
+use List::Util qw(min max sum product);
+use bigint;
+
+my $inputfile = IO::File->new('./input.txt');
+
+my $input = <$inputfile>;
+chomp $input;
+
+my @hex = split '', $input;
+
+my @bits;
+
+sub applyOp { my ($op, @numbers) = @_;
+    if ($op == 0) {
+        sum @numbers
+    } elsif ($op eq 1) {
+        product @numbers
+    } elsif ($op eq 2) {
+        min @numbers
+    } elsif ($op eq 3) {
+        max @numbers
+    } elsif ($op eq 5) {
+        ($numbers[0] > $numbers[1]) || 0
+    } elsif ($op eq 6) {
+        ($numbers[0] < $numbers[1]) || 0
+    } elsif ($op eq 7) {
+        ($numbers[0] == $numbers[1]) || 0
+    }
+}
+
+while (defined (my $hex = shift @hex)) {
+    push @bits, split '', sprintf('%04b', hex $hex)
+}
+
+my $parse = recursive {
+    my $limitType = shift;
+    my $limit = shift;
+
+    my @numbers;
+
+    my $bitsParsed = 0;
+    my $packetsParsed = 0;
+
+    while (@bits > 6) {
+        my $cond1 = (defined($limitType) && $limitType == 0) ? $bitsParsed < $limit : 1;
+        my $cond2 = (defined($limitType) && $limitType == 1) ? $packetsParsed < $limit : 1;
+        last unless $cond1 && $cond2;
+
+        my $version = oct '0b' . join '', splice @bits, 0, 3;
+        $bitsParsed += 3;
+
+        my $typeID = oct '0b' . join '', splice @bits, 0, 3;
+        $bitsParsed += 3;
+
+        if ($typeID == 4) {
+            my $flag = 1;
+            my $number = '';
+            while ($flag) {
+                $flag = shift @bits;
+                $number .= join '', splice @bits, 0, 4;
+                $bitsParsed += 5;
+            }
+            $number = oct '0b' . $number;
+            push @numbers, $number;
+        } else {
+            my $lengthTypeID = shift @bits;
+            $bitsParsed += 1;
+
+            my @recursiveResult;
+            if ($lengthTypeID == 0) {
+                my $lengthOfAllSubpackets = oct '0b' . join '', splice @bits, 0, 15;
+                $bitsParsed += 15;
+                @recursiveResult = $REC->(0, $lengthOfAllSubpackets);
+            } else {
+                my $numSubpackets = oct '0b' . join '', splice @bits, 0, 11;
+                $bitsParsed += 11;
+                @recursiveResult = $REC->(1, $numSubpackets);
+            }
+            $bitsParsed += pop @recursiveResult;
+            push @numbers, applyOp($typeID, @recursiveResult);
+        }
+    } continue {++$packetsParsed}
+    (@numbers, $bitsParsed)
+};
+
+say(($parse->())[0])
