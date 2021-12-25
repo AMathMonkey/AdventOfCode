83ef85309f8ee0ae2bb9103bfb10e6cf3ece3332
AMathMonkey <amathmonkey@gmail.com>
2021-12-25 01:03:16 -0500

    Minor efficiency improvement and code style fix Use array of subs instead of if-elses Don't do unnecessary unpacking of offsets

--- day19/day19p2.pl
+++ day19/day19p2.pl
@@ -23,9 +23,9 @@ foreach my $line (@input) {
 sub findTranslation { my ($scanner1, $scanner2) = @_;
     foreach my $beacon1 (@$scanner1) {
         foreach my $beacon2 (@$scanner2) {
-            my ($xOffset, $yOffset, $zOffset) = offsets($beacon1, $beacon2);
-            my $matches = beaconsInCommon($scanner1, translate($scanner2, $xOffset, $yOffset, $zOffset));
-            return ($xOffset, $yOffset, $zOffset) if $matches >= 12;
+            my @offsets = offsets($beacon1, $beacon2);
+            my $matches = beaconsInCommon($scanner1, translate($scanner2, @offsets));
+            return @offsets if $matches >= 12;
         }
     }
     ()
@@ -38,35 +38,35 @@ sub translate { my ($scanner, $tx, $ty, $tz) = @_;
     } @$scanner]
 }
 
+my @rotNumToRotator = (
+    sub {my ($x, $y, $z) = @_; [$x, $y, $z]},
+    sub {my ($x, $y, $z) = @_; [-$x, -$y, $z]},
+    sub {my ($x, $y, $z) = @_; [-$x, $y, -$z]}, 
+    sub {my ($x, $y, $z) = @_; [$x, -$y, -$z]},
+    sub {my ($x, $y, $z) = @_; [-$x, $z, $y]},
+    sub {my ($x, $y, $z) = @_; [$x, $z, -$y]},
+    sub {my ($x, $y, $z) = @_; [$x, -$z, $y]},
+    sub {my ($x, $y, $z) = @_; [-$x, -$z, -$y]},
+    sub {my ($x, $y, $z) = @_; [-$y, $x, $z]},
+    sub {my ($x, $y, $z) = @_; [$y, -$x, $z]},
+    sub {my ($x, $y, $z) = @_; [$y, $x, -$z]},
+    sub {my ($x, $y, $z) = @_; [-$y, -$x, -$z]},
+    sub {my ($x, $y, $z) = @_; [$y, $z, $x]},
+    sub {my ($x, $y, $z) = @_; [-$y, -$z, $x]},
+    sub {my ($x, $y, $z) = @_; [-$y, $z, -$x]},
+    sub {my ($x, $y, $z) = @_; [$y, -$z, -$x]},
+    sub {my ($x, $y, $z) = @_; [$z, $x, $y]},
+    sub {my ($x, $y, $z) = @_; [-$z, -$x, $y]},
+    sub {my ($x, $y, $z) = @_; [-$z, $x, -$y]},
+    sub {my ($x, $y, $z) = @_; [$z, -$x, -$y]},
+    sub {my ($x, $y, $z) = @_; [-$z, $y, $x]},
+    sub {my ($x, $y, $z) = @_; [$z, -$y, $x]},
+    sub {my ($x, $y, $z) = @_; [$z, $y, -$x]},
+    sub {my ($x, $y, $z) = @_; [-$z, -$y, -$x]}
+);
+
 sub rotate { my ($scanner, $rotNum) = @_; 
-    [map {
-        my ($x, $y, $z) = @$_;
-        if ($rotNum == 0) {[$x, $y, $z]}
-        elsif ($rotNum == 1) {[-$x, -$y, $z]}
-        elsif ($rotNum == 2) {[-$x, $y, -$z]} 
-        elsif ($rotNum == 3) {[$x, -$y, -$z]} 
-        elsif ($rotNum == 4) {[-$x, $z, $y]} 
-        elsif ($rotNum == 5) {[$x, $z, -$y]} 
-        elsif ($rotNum == 6) {[$x, -$z, $y]} 
-        elsif ($rotNum == 7) {[-$x, -$z, -$y]} 
-        elsif ($rotNum == 8) {[-$y, $x, $z]} 
-        elsif ($rotNum == 9) {[$y, -$x, $z]} 
-        elsif ($rotNum == 10) {[$y, $x, -$z]} 
-        elsif ($rotNum == 11) {[-$y, -$x, -$z]} 
-        elsif ($rotNum == 12) {[$y, $z, $x]} 
-        elsif ($rotNum == 13) {[-$y, -$z, $x]} 
-        elsif ($rotNum == 14) {[-$y, $z, -$x]} 
-        elsif ($rotNum == 15) {[$y, -$z, -$x]} 
-        elsif ($rotNum == 16) {[$z, $x, $y]} 
-        elsif ($rotNum == 17) {[-$z, -$x, $y]} 
-        elsif ($rotNum == 18) {[-$z, $x, -$y]} 
-        elsif ($rotNum == 19) {[$z, -$x, -$y]} 
-        elsif ($rotNum == 20) {[-$z, $y, $x]} 
-        elsif ($rotNum == 21) {[$z, -$y, $x]} 
-        elsif ($rotNum == 22) {[$z, $y, -$x]} 
-        elsif ($rotNum == 23) {[-$z, -$y, -$x]} 
-        else {die "$rotNum is not between 0 and 23\n"}
-    } $scanner->@*]
+    [map {$rotNumToRotator[$rotNum]->(@$_)} @$scanner]
 }
 
 sub beaconRepr { my ($beacon) = @_;
