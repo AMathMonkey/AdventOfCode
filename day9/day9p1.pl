15ac9605ce7d4eb8daa00d867d9d56d399807c30
AMathMonkey <amathmonkey@gmail.com>
2021-12-26 23:54:12 -0500

    Speed up day 9 quite a bit using more PDL threading

--- day9/day9p1.pl
+++ day9/day9p1.pl
@@ -4,7 +4,7 @@ use Data::Dumper;
 use v5.28.1;
 use PDL;
 use PDL::NiceSlice;
-use PDL::AutoLoader;
+
 
 my $inputfile = IO::File->new('./input.txt');
 
@@ -12,30 +12,19 @@ my @input = <$inputfile>;
 chomp @input;
 
 my $grid = pdl map {[split '', $_]} @input;
+my $g = $grid->copy;
 
 $grid->whereND($grid == 0) .= -1;
 
-$grid = $grid->range(ndcoords($grid) - 1, 3, 'truncate');
+$grid = $grid->range(ndcoords($grid) - 1, 3, 'truncate')->reorder(2,3,0,1);
 
 $grid->where($grid == 0) .= 9;
 $grid->where($grid == -1) .= 0;
 
-my ($rows, $cols, $gridrows, $gridcols) = dims $grid;
+my $result = zeroes((dims $grid)[2..3]);
 
-my $result = zeroes $rows, $cols;
+my $mins = minimum minimum $grid;
+$result->where($mins == $g) .= $mins->where($mins == $g) + 1;
 
-for (my $i = 0; $i < $rows; ++$i) {
-    for (my $j = 0; $j < $cols; ++$j) {
-        my $min = 9;
-        for (my $gi = 0; $gi < $gridrows; ++$gi) {
-            for (my $gj = 0; $gj < $gridcols; ++$gj) {
-                my $elem = sclr $grid($i, $j, $gi, $gj);
-                $min = $elem if $elem < $min
-            }
-        }
-        my $middle = sclr $grid($i, $j, 1, 1);
-        $result($i, $j) .= $middle + 1 if $middle == $min;
-    }
-}
 
 say sum $result;
