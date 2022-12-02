9739944adfdb4fee0b9f706e91ba7f3e159913a7
AMathMonkey <amathmonkey@gmail.com>
2022-12-02 10:38:30 -0500

    Add day 2 solutions in Tcl

---
+++ 2022/day2/day2p1.tcl
@@ -0,0 +1,21 @@
+set input [open input.txt]
+
+set decrypter [dict create A rock B paper C scissors X rock Y paper Z scissors]
+set pointMapping [dict create rock 1 paper 2 scissors 3 win 6 draw 3]
+set winnerMapping [dict create rock scissors paper rock scissors paper]
+
+while {[gets $input line] >= 0} {
+    lappend rounds [lmap letter [split $line " "] {dict get $decrypter $letter}] 
+}
+
+set points 0
+foreach round $rounds {
+    lassign $round opponent me
+    incr points [dict get $pointMapping $me]
+    if {$me eq $opponent} {
+        incr points [dict get $pointMapping draw]
+    } elseif {[dict get $winnerMapping $me] eq $opponent } {
+        incr points [dict get $pointMapping win]
+    }
+}
+puts $points
---
+++ 2022/day2/day2p2.tcl
@@ -0,0 +1,24 @@
+set input [open input.txt]
+
+set decrypter [dict create A rock B paper C scissors X lose Y draw Z win]
+set pointMapping [dict create rock 1 paper 2 scissors 3 win 6 draw 3 lose 0]
+set winnerMapping [dict create rock scissors paper rock scissors paper]
+
+while {[gets $input line] >= 0} {
+    lappend rounds [lmap letter [split $line " "] {dict get $decrypter $letter}] 
+}
+
+set points 0
+foreach round $rounds {
+    lassign $round opponent outcome
+    if {$outcome eq {lose}} {
+        set me [dict get $winnerMapping $opponent] 
+    } elseif {$outcome eq {draw}} {
+        set me $opponent
+    } else {
+        lassign [dict filter $winnerMapping value $opponent] me
+    }
+    incr points [dict get $pointMapping $me]
+    incr points [dict get $pointMapping $outcome]
+}
+puts $points
