ddeb99aab084b48b0eeb7653370044b759a6399c
AMathMonkey <amathmonkey@gmail.com>
2022-12-04 18:23:31 -0500

    Reduce variable declarations

--- 2022/day4/day4p1.nim
+++ 2022/day4/day4p1.nim
@@ -8,10 +8,8 @@ var count = 0
 for line in lines:
   let
     ranges = line.split(',')
-    range1Str = ranges[0]
-    range2Str = ranges[1]
-    range1Parsed =  range1Str.split('-').map(parseInt)
-    range2Parsed = range2Str.split('-').map(parseInt)
+    range1Parsed = ranges[0].split('-').map(parseInt)
+    range2Parsed = ranges[1].split('-').map(parseInt)
     range1Start = range1Parsed[0]
     range1End = range1Parsed[1]
     range2Start = range2Parsed[0]
--- 2022/day4/day4p2.nim
+++ 2022/day4/day4p2.nim
@@ -8,20 +8,14 @@ var count = 0
 for line in lines:
   let
     ranges = line.split(',')
-    range1Str = ranges[0]
-    range2Str = ranges[1]
-    range1Parsed =  range1Str.split('-').map(parseInt)
-    range2Parsed = range2Str.split('-').map(parseInt)
-    range1Start = range1Parsed[0]
-    range1End = range1Parsed[1]
-    range2Start = range2Parsed[0]
-    range2End = range2Parsed[1]
-    range1 = (range1Start .. range1End)
-    range2 = (range2Start .. range2End)
-  if range1Start in range2 or
-  range1End in range2 or
-  range2Start in range1 or
-  range2End in range1:
+    range1Parsed = ranges[0].split('-').map(parseInt)
+    range2Parsed = ranges[1].split('-').map(parseInt)
+    range1 = (range1Parsed[0] .. range1Parsed[1])
+    range2 = (range2Parsed[0] .. range2Parsed[1])
+  if range1Parsed[0] in range2 or
+  range1Parsed[1] in range2 or
+  range2Parsed[0] in range1 or
+  range2Parsed[1] in range1:
     inc count
 
 echo count
