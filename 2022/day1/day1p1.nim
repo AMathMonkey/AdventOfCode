cdf7d4e74918cc3f2c26cd66b3d9c4c8cc4daa74
AMathMonkey <amathmonkey@gmail.com>
2022-12-04 17:27:37 -0500

    Add day 1 in nim

---
+++ 2022/day1/day1p1.nim
@@ -0,0 +1,15 @@
+import strutils
+import sequtils
+import sugar
+
+let lines = readFile("input.txt").splitLines
+
+iterator getElves(lines: seq[string]): seq[int] = 
+    var currentElf: seq[int] = @[]
+    for line in lines:
+        if line != "": currentElf.add(line.parseInt)
+        else:
+            yield currentElf
+            currentElf = @[]
+
+echo getElves(lines).toSeq.map(x => x.foldl(a + b)).max
---
+++ 2022/day1/day1p2.nim
@@ -0,0 +1,16 @@
+import strutils
+import sequtils
+import sugar
+import algorithm
+
+let lines = readFile("input.txt").splitLines
+
+iterator getElves(lines: seq[string]): seq[int] = 
+    var currentElf: seq[int] = @[]
+    for line in lines:
+        if line != "": currentElf.add(line.parseInt)
+        else:
+            yield currentElf
+            currentElf = @[]
+
+echo getElves(lines).toSeq.map(elf => elf.foldl(a + b)).sorted[^3 .. ^1].foldl(a + b)
