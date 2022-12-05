39e23182aad520511f9b028ebae3e5dc26cce631
AMathMonkey <amathmonkey@gmail.com>
2022-12-05 18:59:57 -0500

    Reverse stack order (end is top now)

--- 2022/day5/day5p1.tcl
+++ 2022/day5/day5p1.tcl
@@ -6,18 +6,16 @@ foreach line $lines {
     break
 }
 
-for {set i 0} {$i < $numStacks} {incr i} {
-    lappend stacks [list]
-}
+for {set i 0} {$i < $numStacks} {incr i} {lappend stacks [list]}
 
-foreach line [lrange $lines 0 $initLines-1] {
+foreach line [lreverse [lrange $lines 0 $initLines-1]] {
     set letters [list]
     for {set i 1} {$i < [string length $line]} {incr i 4} {
         lappend letters [string index $line $i]
     }
     for {set i 0} {$i < [llength $letters]} {incr i} {
         set letter [lindex $letters $i]
-        if {$letter ne " "} {lset stacks $i end+1 $letter}
+        if {$letter ne { }} {lset stacks $i end+1 $letter}
     }
 }
 
@@ -26,12 +24,10 @@ foreach line [lrange $lines $initLines+2 end] {
     for {set i 0} {$i < $num} {incr i} {
         set srcStack [lindex $stacks $src-1]
         set destStack [lindex $stacks $dest-1]
-        set popped [lindex $srcStack 0]
-        lset stacks $src-1 [lreplace $srcStack 0 0]
-        lset stacks $dest-1 [linsert $destStack 0 $popped]
+        set popped [lindex $srcStack end]
+        lset stacks $src-1 [lrange $srcStack 0 end-1]
+        lset stacks $dest-1 [linsert $destStack end $popped]
     }
 }
 
-foreach stack $stacks {
-    puts -nonewline [lindex $stack 0]
-}
+foreach stack $stacks {puts -nonewline [lindex $stack end]}
--- 2022/day5/day5p2.tcl
+++ 2022/day5/day5p2.tcl
@@ -6,30 +6,26 @@ foreach line $lines {
     break
 }
 
-for {set i 0} {$i < $numStacks} {incr i} {
-    lappend stacks [list]
-}
+for {set i 0} {$i < $numStacks} {incr i} {lappend stacks [list]}
 
-foreach line [lrange $lines 0 $initLines-1] {
+foreach line [lreverse [lrange $lines 0 $initLines-1]] {
     set letters [list]
     for {set i 1} {$i < [string length $line]} {incr i 4} {
         lappend letters [string index $line $i]
     }
     for {set i 0} {$i < [llength $letters]} {incr i} {
         set letter [lindex $letters $i]
-        if {$letter ne " "} {lset stacks $i end+1 $letter}
+        if {$letter ne { }} {lset stacks $i end+1 $letter}
     }
 }
 
 foreach line [lrange $lines $initLines+2 end] {
-    regexp {move (\d+) from (\d+) to (\d+)} $line _ num src dest
+    regexp {move (\d+) from (\d+) to (\d+)} $line {} num src dest
     set srcStack [lindex $stacks $src-1]
     set destStack [lindex $stacks $dest-1]
-    set popped [lrange $srcStack 0 $num-1]
-    lset stacks $src-1 [lreplace $srcStack 0 $num-1]
-    lset stacks $dest-1 [linsert $destStack 0 {*}$popped]
+    set popped [lreplace $srcStack 0 end-$num]
+    lset stacks $src-1 [lrange $srcStack 0 end-$num]
+    lset stacks $dest-1 [linsert $destStack end {*}$popped]
 }
 
-foreach stack $stacks {
-    puts -nonewline [lindex $stack 0]
-}
+foreach stack $stacks {puts -nonewline [lindex $stack end]}
