ad613df7f5e216293c0c1902b122be82b213a598
AMathMonkey <amathmonkey@gmail.com>
2022-12-05 22:17:15 -0500

    Add day 5 solutions in nim

---
+++ 2022/day5/day5p2.nim
@@ -0,0 +1,39 @@
+import strutils
+import sequtils
+import sugar
+import re
+
+let
+  lines = readFile("input.txt").splitLines
+var
+  initLines: int
+  numStacks: int
+
+for i, line in lines:
+  if '[' in line: continue
+  let stackNums = line.split(" ").filterIt(it != "")
+  initLines = i
+  numStacks = stackNums[^1].parseInt
+  break
+
+var stacks = newSeq[seq[char]](numStacks)
+
+for i in countdown(initLines - 1, 0):
+  let
+    line = lines[i]
+    letters = collect(newSeq):
+      for i in countup(1, line.len - 1, 4): line[i]
+  for i, letter in letters:
+    if letter != ' ': stacks[i].add(letter)
+
+
+for line in lines[initLines + 2 .. ^1]:
+  if line =~ re"move (\d+) from (\d+) to (\d+)":
+    let
+      num = matches[0].parseInt
+      src = matches[1].parseInt - 1
+      dest = matches[2].parseInt - 1
+    stacks[dest].insert(stacks[src][^num..^1], stacks[dest].len)
+    stacks[src].delete(stacks[src].len - num .. stacks[src].high)
+
+echo stacks.mapIt(it[^1]).join
