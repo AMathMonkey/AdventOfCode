6c12fa72cee87ccbe34eb1226836d00bf657b052
AMathMonkey <amathmonkey@gmail.com>
2022-12-06 22:38:44 -0500

    Add day 6 solution in nim

---
+++ 2022/day6/day6p2.nim
@@ -0,0 +1,18 @@
+import strutils
+
+const markerLength = 14
+
+func toCharSet(s: string): set[char] =
+  for c in s: result.incl(c)
+
+let input = readFile("input.txt")
+
+var buffer = ""
+
+for i, c in input:
+  buffer.add(c)
+  if buffer.len == markerLength:
+    if buffer.toCharSet.card == markerLength:
+      echo i + 1
+      break
+    buffer.delete(0..0)
