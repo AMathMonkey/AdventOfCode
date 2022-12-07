6689e4713796c4ac5f0cf54717450c88ab88f3d1
AMathMonkey <amathmonkey@gmail.com>
2022-12-06 22:48:37 -0500

    Add day 6 solutions in Tcl

---
+++ 2022/day6/day6p2.tcl
@@ -0,0 +1,14 @@
+set markerLength 14
+
+set input [split [read [open input.txt]] {}]
+
+for {set i 0} {$i < [llength $input]} {incr i} {
+    lappend buffer [lindex $input $i]
+    if {[llength $buffer] == $markerLength} {
+        if {[llength [lsort -unique $buffer]] == $markerLength} {
+            puts [expr {$i + 1}]
+            break
+        }
+        set buffer [lrange $buffer 1 end]
+    }
+}
