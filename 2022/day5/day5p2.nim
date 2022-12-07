e1e4a6832d3fdce48fa3190131c45458e51b659e
AMathMonkey <amathmonkey@gmail.com>
2022-12-06 21:08:29 -0500

    Minor simplifications

--- 2022/day5/day5p2.nim
+++ 2022/day5/day5p2.nim
@@ -3,11 +3,8 @@ import sequtils
 import sugar
 import re
 
-let
-  lines = readFile("input.txt").splitLines
-var
-  initLines: int
-  numStacks: int
+let lines = readFile("input.txt").splitLines
+var initLines, numStacks: int
 
 for i, line in lines:
   if '[' in line: continue
@@ -21,7 +18,7 @@ var stacks = newSeq[seq[char]](numStacks)
 for i in countdown(initLines - 1, 0):
   let
     line = lines[i]
-    letters = collect(newSeq):
+    letters = collect:
       for i in countup(1, line.len - 1, 4): line[i]
   for i, letter in letters:
     if letter != ' ': stacks[i].add(letter)
@@ -33,7 +30,7 @@ for line in lines[initLines + 2 .. ^1]:
       num = matches[0].parseInt
       src = matches[1].parseInt - 1
       dest = matches[2].parseInt - 1
-    stacks[dest].insert(stacks[src][^num..^1], stacks[dest].len)
+    stacks[dest].insert(stacks[src][^num .. ^1], stacks[dest].len)
     stacks[src].delete(stacks[src].len - num .. stacks[src].high)
 
 echo stacks.mapIt(it[^1]).join
