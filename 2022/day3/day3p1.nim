04b2e3ebf43431639462a778dad936adfd17a3ae
AMathMonkey <amathmonkey@gmail.com>
2022-12-06 19:30:34 -0500

    Char sets are cool

--- 2022/day3/day3p1.nim
+++ 2022/day3/day3p1.nim
@@ -1,5 +1,7 @@
 import strutils
-import sets
+
+func toCharSet(s: string): set[char] =
+  for c in s: result.incl(c)
 
 func score(letter: char): int =
   letter.ord - (if letter.isUpperAscii: 38 else: 96)
@@ -10,7 +12,7 @@ var total = 0
 
 for line in lines:
   let halfway = line.len div 2
-  for letter in (line[0 ..< halfway].toHashSet) * (line[halfway .. ^1].toHashSet):
+  for letter in (line[0 ..< halfway].toCharSet) * (line[halfway .. ^1].toCharSet):
     total.inc(letter.score)
 
 echo total
--- 2022/day3/day3p2.nim
+++ 2022/day3/day3p2.nim
@@ -1,5 +1,8 @@
 import strutils
-import sets
+import sequtils
+
+func toCharSet(s: string): set[char] =
+  for c in s: result.incl(c)
 
 func score(letter: char): int =
   letter.ord - (if letter.isUpperAscii: 38 else: 96)
@@ -9,7 +12,7 @@ let lines = readFile("input.txt").splitLines
 var total = 0
 
 for i in countup(0, lines.high, 3):
-  for letter in (lines[i].toHashSet) * (lines[i + 1].toHashSet) * (lines[i + 2].toHashSet):
+  for letter in lines[i .. i + 2].map(toCharSet).foldl(a * b):
     total.inc(letter.score)
 
 echo total
