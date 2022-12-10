c06ba6ef495945da1bba2bf45260400974468a2b
AMathMonkey <amathmonkey@gmail.com>
2022-12-09 21:10:35 -0500

    Add day9 solutions in Tcl

---
+++ 2022/day9/day9p1.tcl
@@ -0,0 +1,39 @@
+namespace path {::tcl::mathop ::tcl::mathfunc}
+set input [open input.txt]
+
+set x 0; set y 0
+set tailX 0; set tailY 0
+set tailLocations [list [list 0 0]]
+
+proc moveHead {dir} {
+    switch $dir {
+        U {upvar y y; incr y}
+        D {upvar y y; incr y -1}
+        R {upvar x x; incr x}
+        L {upvar x x; incr x -1}
+    }
+}
+
+proc moveTail {x y tailX tailY} {
+    if {abs($x - $tailX) <= 1 && abs($y - $tailY) <= 1} {return [list $tailX $tailY]}
+
+    if {$x == $tailX && abs($y - $tailY) == 2} {
+        incr tailY [expr {$y > $tailY ? 1 : -1}]
+    } elseif {$y == $tailY && abs($x - $tailX) == 2} {
+        incr tailX [expr {$x > $tailX ? 1 : -1}]
+    } else {
+        incr tailY [expr {$y > $tailY ? 1 : -1}]
+        incr tailX [expr {$x > $tailX ? 1 : -1}]
+    }
+    list $tailX $tailY
+}
+
+while {[gets $input line] >= 0} {
+    lassign $line dir num
+    for {set times 0} {$times < $num} {incr times} {
+        moveHead $dir
+        lassign [moveTail $x $y $tailX $tailY] tailX tailY
+        lappend tailLocations [list $tailX $tailY]
+    }
+} 
+puts [llength [lsort -unique $tailLocations]]
---
+++ 2022/day9/day9p2.tcl
@@ -0,0 +1,45 @@
+namespace path {::tcl::mathop ::tcl::mathfunc}
+set input [open input.txt]
+
+set x 0; set y 0
+set tails [lrepeat 9 [list 0 0]]
+set tailLocations [list [list 0 0]]
+
+proc moveHead {dir} {
+    switch $dir {
+        U {upvar y y; incr y}
+        D {upvar y y; incr y -1}
+        R {upvar x x; incr x}
+        L {upvar x x; incr x -1}
+    }
+}
+
+proc moveTail {x y tailX tailY} {
+    if {abs($x - $tailX) <= 1 && abs($y - $tailY) <= 1} {return [list $tailX $tailY]}
+
+    if {$x == $tailX && abs($y - $tailY) == 2} {
+        incr tailY [expr {$y > $tailY ? 1 : -1}]
+    } elseif {$y == $tailY && abs($x - $tailX) == 2} {
+        incr tailX [expr {$x > $tailX ? 1 : -1}]
+    } else {
+        incr tailY [expr {$y > $tailY ? 1 : -1}]
+        incr tailX [expr {$x > $tailX ? 1 : -1}]
+    }
+    list $tailX $tailY
+}
+
+while {[gets $input line] >= 0} {
+    lassign $line dir num
+    for {set times 0} {$times < $num} {incr times} {
+        moveHead $dir
+        lassign [lindex $tails 0] tempX tempY
+        lset tails 0 [moveTail $x $y $tempX $tempY]
+        for {set tail 1} {$tail < [llength $tails]} {incr tail} {
+            lassign [lindex $tails $tail] tempX tempY
+            lassign [lindex $tails $tail-1] prevX prevY
+            lset tails $tail [moveTail $prevX $prevY $tempX $tempY]
+        }
+        lappend tailLocations [lindex $tails end]
+    }
+} 
+puts [llength [lsort -unique $tailLocations]]
