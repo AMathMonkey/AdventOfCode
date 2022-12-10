9483c48e6940fc1e062c701b8fa755c44d1addd3
AMathMonkey <amathmonkey@gmail.com>
2022-12-10 02:32:31 -0500

    Cleanup of hard-coded numbers

--- 2022/day10/day10p2.tcl
+++ 2022/day10/day10p2.tcl
@@ -13,11 +13,14 @@ proc readline {} {
     }]
 }
 
+set height 6
+set width 40
+set pixels [expr {$height * $width}]
+
 set cycle 1
 set x 1
 set nextop readline
-set screen [lrepeat 240 .] 
-set pixelNum 0
+set screen [lrepeat $pixels .] 
 
 while {$cycle <= 240} {
     if {($cycle + 20) % 40 == 0 && $nextop ne {readline}} {
@@ -33,10 +36,10 @@ while {$cycle <= 240} {
         }
         readline {readline}
     }
-    if {abs(($cycle - 1) % 40 - $x) <= 1} {lset screen $cycle-1 #}
+    if {abs(($cycle - 1) % $width - $x) <= 1} {lset screen $cycle-1 #}
 }
 
-for {set i 0} {$i < 240} {incr i 40} {
-    set line [lrange $screen $i [expr {$i + 40 - 1}]]
+for {set i 0} {$i < $pixels} {incr i $width} {
+    set line [lrange $screen $i [expr {$i + $width - 1}]]
     puts [join $line {}]
 }
