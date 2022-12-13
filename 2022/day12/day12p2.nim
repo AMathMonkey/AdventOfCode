03a7262f8108af3794f36734ee5e2008a45c00b7
AMathMonkey <amathmonkey@gmail.com>
2022-12-12 23:49:43 -0500

    Add day 12 in nim. 5 sec in Tcl vs 50 ms in nim!

---
+++ 2022/day12/day12p2.nim
@@ -0,0 +1,64 @@
+import strutils
+import sequtils
+
+func diff(src, dest: char): int =
+  func transform(chr: char): char = 
+    if chr == 'S': 'a' elif chr == 'E': 'z' else: chr
+  dest.transform.ord - src.transform.ord
+
+let 
+  grid = readFile("input.txt").splitLines
+  rows = grid.len
+  cols = grid[0].len
+
+var
+  resultGrid = newSeqWith(rows, newSeqWith(cols, int.high - 1))
+  prevResultGrid: seq[seq[int]]
+
+while prevResultGrid != resultGrid:
+  prevResultGrid = resultGrid
+  for row in countup(grid.low, grid.high):
+    for col in countup(grid[row].low, grid[row].high):
+      let val = grid[row][col]
+      var curDist = resultGrid[row][col]
+      if val == 'E':
+        resultGrid[row][col] = 0
+        curDist = 0
+      
+      if col - 1 >= grid[row].low:
+        let 
+          valToLeft = grid[row][col - 1]
+          distToLeft = resultGrid[row][col - 1]
+        if distToLeft + 1 < curDist and diff(val, valToLeft) <= 1:
+          curDist = distToLeft + 1
+
+      if col + 1 <= grid[row].high:
+        let 
+          valToRight = grid[row][col + 1]
+          distToRight = resultGrid[row][col + 1]
+        if distToRight + 1 < curDist and diff(val, valToRight) <= 1:
+          curDist = distToRight + 1
+
+      if row - 1 >= grid.low:
+        let 
+          valAbove = grid[row - 1][col]
+          distAbove = resultGrid[row - 1][col]
+        if distAbove + 1 < curDist and diff(val, valAbove) <= 1:
+          curDist = distAbove + 1
+        
+      if row + 1 <= grid.high:
+        let
+          valBelow = grid[row + 1][col]
+          distBelow = resultGrid[row + 1][col]
+        if distBelow + 1 < curDist and diff(val, valBelow) <= 1:
+          curDist = distBelow + 1
+
+      resultGrid[row][col] = curDist
+
+var min = int.high
+for i, row in grid:
+  for j, elem in row:
+    if (elem == 'a' or elem == 'S') and resultGrid[i][j] < min:
+      min = resultGrid[i][j]
+
+echo min
