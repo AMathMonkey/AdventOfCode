ce4b764e58f95693f6f9149831528243e5c36f58
AMathMonkey <amathmonkey@gmail.com>
2022-12-14 02:37:38 -0500

    Add day 14 part 2 solution in Tcl

---
+++ 2022/day14/day14p2.tcl
@@ -0,0 +1,72 @@
+set input [open input.txt]
+
+set minRow 0
+
+set maxRow 0
+set minCol Inf
+set maxCol 0
+while {[gets $input line] >= 0} {
+    set line [split [string map [list { -> } { }] $line] { }]
+    set prevRow {}
+    foreach point $line {
+        lassign [split $point ,] col row
+        if {$row > $maxRow} {set maxRow $row}
+        if {$col < $minCol} {set minCol $col}
+        if {$col > $maxCol} {set maxCol $col}
+
+        if {$prevRow ne {}} {
+            if {$prevCol == $col} {
+                if {$row - $prevRow > 0} {
+                    set sign 1
+                    set op <=
+                } else {
+                    set sign -1
+                    set op >=
+                }
+                for {} "\$prevRow $op \$row" {incr prevRow $sign} {
+                    set grid($prevRow,$col) #
+                }
+            } else {
+                if {$col - $prevCol > 0} {
+                    set sign 1
+                    set op <=
+                } else {
+                    set sign -1
+                    set op >=
+                }
+                for {} "\$prevCol $op \$col" {incr prevCol $sign} {
+                    set grid($row,$prevCol) #
+                }
+            }
+        }
+        set prevRow $row
+        set prevCol $col
+    }
+}
+
+incr maxRow 2
+
+for {set col 0} {$col < 2 * $maxCol} {incr col} {
+    set grid($maxRow,$col) #
+}
+
+while {![info exists grid(0,500)]} {
+    set sandRow 0
+    set sandCol 500
+    
+    while {true} {
+        if {![info exists grid([expr {$sandRow + 1}],$sandCol)]} {
+            incr sandRow
+        } elseif {![info exists grid([expr {$sandRow + 1}],[expr {$sandCol - 1}])]} {
+            incr sandRow
+            incr sandCol -1
+        } elseif {![info exists grid([expr {$sandRow + 1}],[expr {$sandCol + 1}])]} {
+            incr sandRow
+            incr sandCol
+        } else {break}
+    }
+    set grid($sandRow,$sandCol) o
+    incr result
+}
+
+puts $result
