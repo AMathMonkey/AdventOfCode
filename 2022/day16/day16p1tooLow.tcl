911e261ea80c3e56fb5be5d19cbec9af6107a11e
AMathMonkey <amathmonkey@gmail.com>
2022-12-16 19:34:22 -0500

    Code does not work for example or actual data

---
+++ 2022/day16/day16p1tooLow.tcl
@@ -0,0 +1,50 @@
+set input [open input.txt]
+
+set timeLimit 30
+
+while {[gets $input line] >= 0} {
+    regexp {Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)} $line {} valve rate tunnels
+    set valves($valve,rate) $rate
+    set valves($valve,tunnels) [split [string map [list {, } ,] $tunnels] ,]
+}
+
+proc getMax {valve openValves timeElapsed} {
+    global valves cache timeLimit
+
+    if {$timeElapsed == $timeLimit} {return 0}
+
+    set openValves [lsort $openValves]
+    set cacheIndex "$valve,$openValves,$timeElapsed"
+
+    if {[info exists cache($cacheIndex)]} {return $cache($cacheIndex)}
+
+    incr timeElapsed
+
+    set max -Inf
+
+    set pressureReleased 0
+    foreach openValve $openValves {incr pressureReleased $valves($openValve,rate)}
+
+    foreach destValve $valves($valve,tunnels) {
+        set val [getMax $destValve $openValves $timeElapsed]
+        incr val $pressureReleased
+        if {$val > $max} {set max $val}
+    }
+
+    if {$valves($valve,rate) > 0 && $timeElapsed < $timeLimit && $valve ni $openValves} {
+        incr pressureReleased $pressureReleased
+        set openValves [lsort [concat $openValves $valve]]
+        incr timeElapsed
+
+        foreach destValve $valves($valve,tunnels) {
+            set val [getMax $destValve $openValves $timeElapsed]
+            incr val $pressureReleased
+            if {$val > $max} {set max $val}
+        }
+    }
+
+    set cache($cacheIndex) $max
+    return $max
+}
+
+puts [getMax AA [list] 0]
