9ae692367bc20521245f18455c38a061acf0fe10
AMathMonkey <amathmonkey@gmail.com>
2022-12-26 18:45:32 -0500

    Julia speedup, beats Tcl slightly in p2!

--- 2022/day16/day16p1.jl
+++ 2022/day16/day16p1.jl
@@ -1,29 +1,28 @@
-# Still slower than my Tcl implementation but not too bad. Takes <8 seconds.
+# Still slower than my Tcl implementation but not too bad. Takes <5 seconds. 
+# Was closer to 8 seconds before I added type annotations, so they do speed things up.
 
-import Base.@kwdef
+struct Valve
+    name::String
+    rate::Int
+    tunnels::Vector{String}
+end
 
 timelimit = 30
 
-valves = Dict()
-cache = Dict()
-
-@kwdef struct Valve
-    name
-    rate
-    tunnels
-end
+valves::Dict{String, Valve} = Dict()
+cache::Dict{String, Int} = Dict()
 
 for line in eachline("input.txt")
     @something (m = match(r"Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)", line)) continue
     (name, rate, tunnels) = m
     valves[name] = Valve(
-        name=name,
-        rate=parse(Int, rate),
-        tunnels=split(tunnels, ", "),
+        name,
+        parse(Int, rate),
+        split(tunnels, ", "),
     )
 end
 
-function getmax(valve, openvalves, timeremaining)
+function getmax(valve::Valve, openvalves::Vector{String}, timeremaining::Int)
     timeremaining == 0 && return 0
 
     cacheindex = "$(valve.name),$(join(openvalves, ' ')),$timeremaining"
@@ -47,4 +46,4 @@ function getmax(valve, openvalves, timeremaining)
     return cache[cacheindex] = maxval
 end
 
-println(getmax(valves["AA"], [], timelimit))
+println(getmax(valves["AA"], String[], timelimit))
--- 2022/day16/day16p2.jl
+++ 2022/day16/day16p2.jl
@@ -1,29 +1,29 @@
 # Runs in under 4 minutes. Not as good as my Tcl implementation but eons better than Raku.
+# Correction, after adding type annotations, it runs in just over 2 minutes, beating Tcl!
+# Type annotations are very important for speedup.
 
-import Base.@kwdef
+struct Valve
+    name::String
+    rate::Int
+    tunnels::Vector{String}
+end
 
 timelimit = 26
 
-valves = Dict()
-cache = Dict()
-
-@kwdef struct Valve
-    name
-    rate
-    tunnels
-end
+valves::Dict{String, Valve} = Dict()
+cache::Dict{String, Int} = Dict()
 
 for line in eachline("input.txt")
     @something (m = match(r"Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)", line)) continue
     (name, rate, tunnels) = m
     valves[name] = Valve(
-        name=name,
-        rate=parse(Int, rate),
-        tunnels=split(tunnels, ", "),
+        name,
+        parse(Int, rate),
+        split(tunnels, ", "),
     )
 end
 
-function getmax(valve, openvalves, timeremaining, who)
+function getmax(valve::Valve, openvalves::Vector{String}, timeremaining::Int, who::Symbol)
     timeremaining == 0 && return who == :me ? getmax(valves["AA"], openvalves, timelimit, :elephant) : 0
 
     cacheindex = "$(valve.name),$(join(openvalves, ' ')),$timeremaining,$who"
@@ -47,4 +47,4 @@ function getmax(valve, openvalves, timeremaining, who)
     return cache[cacheindex] = maxval
 end
 
-println(getmax(valves["AA"], [], timelimit, :me))
+println(getmax(valves["AA"], String[], timelimit, :me))
