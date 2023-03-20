b52de643c28dd0789e0a75263f203c7c809a08be
AMathMonkey <amathmonkey@gmail.com>
2023-03-20 01:36:59 -0400

    Day 18 part 1 solution in Tcl (very easy)

---
+++ 2022/day18/day18p1.tcl
@@ -0,0 +1,21 @@
+set input [open input.txt]
+
+while {[gets $input line] >= 0} {
+    set cubes($line) {}
+}
+
+foreach cube [array names cubes] {
+    lassign [split $cube ,] x y z
+    foreach neighbour [list \
+        [expr $x+1],$y,$z \
+        [expr $x-1],$y,$z \
+        $x,[expr $y+1],$z \
+        $x,[expr $y-1],$z \
+        $x,$y,[expr $z+1] \
+        $x,$y,[expr $z-1] \
+    ] {
+        if {![info exists cubes($neighbour)]} {incr result}
+    }
+} 
+
+puts $result
