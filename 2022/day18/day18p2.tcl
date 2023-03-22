53b09b630cd88a363453640181654c4d629b5f12
AMathMonkey <amathmonkey@gmail.com>
2023-03-21 22:59:33 -0400

    Add day18p2 solution in Tcl (takes 3-4 seconds to run)

---
+++ 2022/day18/day18p2.tcl
@@ -0,0 +1,71 @@
+set input [open input.txt]
+
+while {[gets $input line] >= 0} {
+    set cubes($line) {}
+}
+
+set maxX -1
+set maxY -1
+set maxZ -1
+# find max values of x y and z so steam can stop expanding after max + 1 of each axis
+foreach cube [array names cubes] {
+    lassign [split $cube ,] x y z
+    if {$x > $maxX} {set maxX $x}
+    if {$y > $maxY} {set maxY $y}
+    if {$z > $maxZ} {set maxZ $z}
+}
+incr maxX
+incr maxY
+incr maxZ
+
+set lastSize 0
+set steam(-1,-1,-1) {}
+
+# expand steam to fill area around lava droplet until no room is left
+while {$lastSize != [set newSize [array size steam]]} {
+    foreach steamCube [array names steam] {
+        lassign [split $steamCube ,] x y z
+
+        set x1 [expr $x+1]
+        set index $x1,$y,$z
+        if {$x1 <= $maxX && ![info exists cubes($index)]} {set steam($index) {}}
+
+        set xm1 [expr $x-1]
+        set index $xm1,$y,$z
+        if {$xm1 >= -1 && ![info exists cubes($index)]} {set steam($index) {}}
+
+        set y1 [expr $y+1]
+        set index $x,$y1,$z
+        if {$y + 1 <= $maxY && ![info exists cubes($index)]} {set steam($index) {}}
+
+        set ym1 [expr $y-1]
+        set index $x,$ym1,$z
+        if {$ym1 >= -1 && ![info exists cubes($index)]} {set steam($index) {}}
+
+        set z1 [expr $z+1]
+        set index $x,$y,$z1
+        if {$z1 <= $maxZ && ![info exists cubes($index)]} {set steam($index) {}}
+
+        set zm1 [expr $z-1]
+        set index $x,$y,$zm1
+        if {$zm1 >= -1 && ![info exists cubes($index)]} {set steam($index) {}}
+    }
+    set lastSize $newSize
+}
+
+# count cube faces that are in contact with steam 
+foreach cube [array names cubes] {
+    lassign [split $cube ,] x y z
+    foreach neighbour [list \
+        [expr $x+1],$y,$z \
+        [expr $x-1],$y,$z \
+        $x,[expr $y+1],$z \
+        $x,[expr $y-1],$z \
+        $x,$y,[expr $z+1] \
+        $x,$y,[expr $z-1] \
+    ] {
+        if {[info exists steam($neighbour)]} {incr result}
+    }
+} 
+
+puts $result
