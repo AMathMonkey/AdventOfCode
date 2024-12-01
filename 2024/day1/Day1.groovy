03161ddf0668c2a423a33ec09226512800140458
AMathMonkey <amathmonkey@gmail.com>
2024-12-01 17:59:54 -0500

    Day 1 solution in Groovy

---
+++ 2024/day1/Day1.groovy
@@ -0,0 +1,16 @@
+class Day1 {
+
+    static void main(String[] args) {
+        List pairs = ('input.txt' as File).readLines().collect {
+            it.split()*.toInteger()
+        }.transpose().each {it.sort()}.transpose()
+        int diff = pairs.sum { left, right -> Math.abs(left - right) }
+
+        Map rightCounts = pairs.countBy { left, right -> right }
+        int score = pairs.sum { left, right ->
+            left * (rightCounts[left] ?: 0)
+        }
+        println("Part 1: ${diff}\nPart 2: ${score}")
+    }
+
+}
