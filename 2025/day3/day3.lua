f5cd8ec0573ecf9e8fc2eb6f29e7adc65660eee5
AMathMonkey <amathmonkey@gmail.com>
2025-12-03 03:05:38 -0500

    2025 day 3 part 2 solution that takes 15 minutes to run

--- 2025/day3/day3.lua
+++ 2025/day3/day3.lua
@@ -1,6 +1,8 @@
 local input = io.open("input.txt")
 local res1 = 0
-for line in input:lines() do
+local res2 = 0
+
+local function part1(line)
     local counts = {}
     local max = 0
     for c in line:gmatch("%d") do
@@ -19,6 +21,36 @@ for line in input:lines() do
             end
         end
     end
-    res1 = res1 + max
+    return max
 end
+
+local function part2(line)
+    line = line:match("(%d+)")
+    local max = line:sub(1, 12)
+    local function recur(acc, i)
+        if #acc == 12 then
+            if acc > max then
+                max = acc 
+            end
+            return
+        end
+        if max:sub(1, #acc) > acc then
+            return
+        end
+        if i > #line then
+            return
+        end
+        recur(acc .. line:sub(i, i, 1), i + 1)
+        recur(acc, i + 1)
+    end
+    recur("", 1)
+    return math.tointeger(max)
+end
+
+for line in input:lines() do
+    res1 = res1 + part1(line)
+    res2 = res2 + part2(line)
+end
+
 print("Part 1:", res1)
+print("Part 2:", res2)
