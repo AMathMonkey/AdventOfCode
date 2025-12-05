a5e75fbbfe40a43d2715bd07d66f07ff3c9dc4a6
AMathMonkey <amathmonkey@gmail.com>
2025-12-05 11:50:08 -0500

    2025 day 5 full solution in Lua
    Coded entirely in nano

--- 2025/day5/day5.lua
+++ 2025/day5/day5.lua
@@ -16,7 +16,8 @@ while true do
         break
     end
     for _, range in ipairs(ranges) do
-        if line >= range[1] and line <= range[2] then
+        local start, end_ = table.unpack(range)
+        if line >= start and line <= end_ then
             res1 = res1 + 1
             break
         end
@@ -24,3 +25,49 @@ while true do
 end
 
 print("Part 1:", res1)
+
+local function mergeRange(ranges)
+    local function replaceRanges(r1, r2, new)
+        local newRanges = {}
+        for i, range in ipairs(ranges) do
+            if i ~= r1 and i ~= r2 then
+                table.insert(newRanges, range)
+            end
+        end
+        if new then
+            table.insert(newRanges, new)
+        end
+        return newRanges
+    end
+    for i, range in ipairs(ranges) do
+        local start, end_ = table.unpack(range)
+        for j, range2 in ipairs(ranges) do
+            if i ~= j then
+                local start2, end2 = table.unpack(range2)
+                local rangeStartsDuring = start >= start2 and start <= end2
+                local rangeEndsDuring = end_ >= start2 and end_ <= end2
+                if rangeStartsDuring and rangeEndsDuring then
+                    return replaceRanges(i), true
+                elseif rangeStartsDuring and not rangeEndsDuring then
+                    return replaceRanges(i, j, {start2, end_}), true
+                elseif rangeEndsDuring then
+                    return replaceRanges(i, j, {start, end2}), true
+                end
+            end
+        end
+    end
+    return ranges, false
+end
+
+local hadEffect = true
+while hadEffect do
+    ranges, hadEffect = mergeRange(ranges)
+end
+
+local res2 = 0
+for _, range in ipairs(ranges) do
+    local start, end_ = table.unpack(range)
+    res2 = res2 + end_ - start + 1
+end
+
+print("Part 2:", res2)
