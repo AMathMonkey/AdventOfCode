3afa3b53b2b8b44806718a809b9cd4d8661e4217
AMathMonkey <amathmonkey@gmail.com>
2025-12-04 23:33:13 -0500

    2025 day 4 full solution in Lua

--- 2025/day4/day4.lua
+++ 2025/day4/day4.lua
@@ -2,6 +2,7 @@
 local input <close> = io.open("input.txt")
 local lines = {}
 local res1 = 0
+local res2 = 0
 for line in input:lines() do
     local split = {}
     for c in line:gmatch("%S") do
@@ -9,22 +10,50 @@ for line in input:lines() do
     end
     table.insert(lines, split)
 end
+
+local function isAccessible(lines, i, j)
+    local count = 0
+    for shifti = -1, 1 do
+        for shiftj = -1, 1 do
+            if (lines[i + shifti] or {})[j + shiftj] == '@' and (shifti ~= 0 or shiftj ~= 0) then
+                count = count + 1
+            end
+        end
+    end
+    return count < 4
+end
+
 for i = 1, #lines do
     for j = 1, #lines[1] do
         if lines[i][j] == '@' then
-            local count = 0
-            for shifti = -1, 1 do
-                for shiftj = -1, 1 do
-                    if (lines[i + shifti] or {})[j + shiftj] == '@' and (shifti ~= 0 or shiftj ~= 0) then
-                        count = count + 1
-                    end
-                end
-            end
-            if count < 4 then
+            if isAccessible(lines, i, j) then
                 res1 = res1 + 1
             end
         end
     end
 end
 
+while true do
+    local flag = false
+    local newLines = {}
+    for i = 1, #lines do
+        local newLine = {}
+        table.insert(newLines, newLine)
+        for j = 1, #lines[1] do
+            if lines[i][j] == '@' and isAccessible(lines, i, j) then
+                res2 = res2 + 1
+                table.insert(newLine, '.')
+                flag = true
+            else
+                table.insert(newLine, lines[i][j])        
+            end
+        end
+    end
+    lines = newLines
+    if not flag then
+        break
+    end
+end
+
 print("Part 1:", res1)
+print("Part 2:", res2)
