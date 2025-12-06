6499bbc28ea96df8f0df5772fc88de8194875ed2
AMathMonkey <amathmonkey@gmail.com>
2025-12-06 13:07:25 -0500

    2025 day 6 full solution in Lua

--- 2025/day6/day6.lua
+++ 2025/day6/day6.lua
@@ -1,8 +1,12 @@
 local input <close> = io.open("input.txt")
+local lines = {}
+for line in input:lines() do
+    table.insert(lines, line)
+end
 
 local rows = {}
 local ops = {}
-for line in input:lines() do
+for _, line in ipairs(lines) do
     local nums = {}
     for num in line:gmatch("%d+") do
         table.insert(nums, math.tointeger(num))
@@ -16,27 +20,62 @@ for line in input:lines() do
     end
 end
 
-local function add(a, b)
-    return a + b
-end
+local getOpfunc = (function()
+    local function add(a, b)
+        return a + b
+    end
 
-local function mult(a, b)
-    return a * b
-end
+    local function mult(a, b)
+        return a * b
+    end
+
+    return function(op)
+        if op == "*" then
+            return mult
+        else
+            return add
+        end
+    end
+end)()
 
 local res1 = 0
-for col = 1, #rows[1] do
-    local opfunc
-    if ops[col] == "*" then
-        opfunc = mult
-    else
-        opfunc = add
-    end
-    local colRes = rows[1][col]
-    for row = 2, #rows do
-        colRes = opfunc(colRes, rows[row][col])
+for colNum = 1, #rows[1] do
+    local opfunc = getOpfunc(ops[colNum])
+    local colRes = rows[1][colNum]
+    for rowNum = 2, #rows do
+        colRes = opfunc(colRes, rows[rowNum][colNum])
     end
     res1 = res1 + colRes
 end
 
 print("Part 1:", res1)
+
+local res2 = 0
+local nums = {}
+for colNum = #lines[1], 1, -1 do
+    local curNum = ""
+    local function maybeAddCurNum()
+        if #curNum > 0 then
+            table.insert(nums, math.tointeger(curNum))
+        end
+        curNum = ""
+    end
+    for _, line in ipairs(lines) do
+        local char = line:sub(colNum, colNum) 
+        if char:match("%d") then
+            curNum = curNum .. char
+        elseif char:match("[*+]") then
+            maybeAddCurNum()
+            local thisRes = nums[1]
+            local opfunc = getOpfunc(char)
+            for i = 2, #nums do
+                thisRes = opfunc(thisRes, nums[i])
+            end
+            res2 = res2 + thisRes
+            nums = {}
+        end
+    end
+    maybeAddCurNum()
+end
+
+print("Part 2:", res2)
