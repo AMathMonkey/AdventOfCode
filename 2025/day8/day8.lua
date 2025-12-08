43535c3999332b28017eb80d5135c9ab24eb4dbe
AMathMonkey <amathmonkey@gmail.com>
2025-12-08 16:09:52 -0500

    Cleanup (limit scope of helper functions)

--- 2025/day8/day8.lua
+++ 2025/day8/day8.lua
@@ -22,73 +22,77 @@ table.sort(dists, function (d1, d2) return d1.dist < d2.dist end)
 
 local groups = {}
 
-local function findExistingGroup(distPair)
-    for _, g in ipairs(groups) do
-        if g[distPair.n1] or g[distPair.n2] then
-            return g
+local addDistPair = (function()
+    local function findExistingGroup(distPair)
+        for _, g in ipairs(groups) do
+            if g[distPair.n1] or g[distPair.n2] then
+                return g
+            end
         end
     end
-end
-
-local function addDistPair(distPair)
-    local group = findExistingGroup(distPair)
-    if group then
-        if not group[distPair.n1] then
-            group.n = group.n + 1
-            group[distPair.n1] = true
+    
+    return function(distPair)
+        local group = findExistingGroup(distPair)
+        if group then
+            if not group[distPair.n1] then
+                group.n = group.n + 1
+                group[distPair.n1] = true
+            end
+            if not group[distPair.n2] then
+                group.n = group.n + 1
+                group[distPair.n2] = true
+            end
+        else
+            group = {n = 2, [distPair.n1] = true, [distPair.n2] = true}
+            table.insert(groups, group)
         end
-        if not group[distPair.n2] then
-            group.n = group.n + 1
-            group[distPair.n2] = true
-        end
-    else
-        group = {n = 2, [distPair.n1] = true, [distPair.n2] = true}
-        table.insert(groups, group)
     end
-end
+end)()
 
 for i = 1, 1000 do
     addDistPair(dists[i])
 end
 
-local function mergeGroups(groups, n1, n2)
-    local newGroups = {}
-    for n, g in ipairs(groups) do
-        if not (n == n1 or n == n2) then
-            table.insert(newGroups, g)
+local tryMergeGroups = (function()
+    local function mergeGroups(groups, n1, n2)
+        local newGroups = {}
+        for n, g in ipairs(groups) do
+            if not (n == n1 or n == n2) then
+                table.insert(newGroups, g)
+            end
         end
-    end
-    local newGroup = {n = 0}
-    for k in pairs(groups[n1]) do
-        if k ~= 'n' and not newGroup[k] then
-            newGroup[k] = true
-            newGroup.n = newGroup.n + 1
+        local newGroup = {n = 0}
+        for k in pairs(groups[n1]) do
+            if k ~= 'n' and not newGroup[k] then
+                newGroup[k] = true
+                newGroup.n = newGroup.n + 1
+            end
         end
-    end
-    for k in pairs(groups[n2]) do
-        if k ~= 'n' and not newGroup[k] then
-            newGroup[k] = true
-            newGroup.n = newGroup.n + 1
+        for k in pairs(groups[n2]) do
+            if k ~= 'n' and not newGroup[k] then
+                newGroup[k] = true
+                newGroup.n = newGroup.n + 1
+            end
         end
+        table.insert(newGroups, newGroup)
+        return newGroups
     end
-    table.insert(newGroups, newGroup)
-    return newGroups
-end
 
-local function tryMergeGroups(groups)
-    for i = 1, #groups do
-        local group1 = groups[i]
-        for j = i + 1, #groups do
-            local group2 = groups[j]
-            for k in pairs(group1) do
-                if k ~= 'n' and group2[k] then
-                    return mergeGroups(groups, i, j), true
-                end
-            end 
+    return function(groups)
+        for i = 1, #groups do
+            local group1 = groups[i]
+            for j = i + 1, #groups do
+                local group2 = groups[j]
+                for k in pairs(group1) do
+                    if k ~= 'n' and group2[k] then
+                        return mergeGroups(groups, i, j), true
+                    end
+                end 
+            end
         end
+        return groups, false
     end
-    return groups, false
-end
+end)()
 
 local hadEffect = true
 while hadEffect do
